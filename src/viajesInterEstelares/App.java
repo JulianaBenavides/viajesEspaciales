801338b498aec52ebc3a374d46e8bd404babf703
Juliana Benavides Reales <jbenavides09@misena.edu.co>
2025-11-27 22:51:59 -0500

    modificaciones

--- src/viajesInterEstelares/App.java
+++ src/viajesInterEstelares/App.java
@@ -1,5 +1,9 @@
-package viajesInterEstelares;
+import viajesInterEstelares.ViajesInterEstelares;
 
 public class App {
-    
+    public static void main(String[] args) throws Exception {
+        
+    ViajesInterEstelares.travel();    
+
+    }
 }
